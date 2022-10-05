f642147af6c9fed2f3ee5137c19d4680249e6dfc
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-10-05 16:02:30 -0300

    Update 01-gradingStudents.js

--- 02-Implementation/01-gradingStudents.js
+++ 02-Implementation/01-gradingStudents.js
@@ -3,7 +3,6 @@
 function gradingStudents(grades) {
   // Write your code here
   for (let i = 0; i < grades.length; i++) {
-    console.log(grades[i] % 5);
     let rest = grades[i] % 5;
     rest >= 3 && grades[i] > 37 ? (grades[i] = grades[i] + 5 - rest) : null;
   }
